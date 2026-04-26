17e261cfeb358eabbef74882893ebbfd811806c8
Abdul Majeed <abdulmajeedbaloch315@gmail.com>
2026-04-26 09:47:33 +0500

    Mongodb atlas uri connected

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connectDB(uri) {
-  const mongoUri = uri || process.env.MONGODB_URI || 'mongodb://localhost:27017/timeoff_db';
+  const mongoUri = uri || process.env.MONGO_URI || 'mongodb://localhost:27017/timeoff_db';
   await mongoose.connect(mongoUri);
-  console.log(`[DB] Connected to MongoDB: ${mongoUri}`);
+  console.log(`[DB] Connected to MongoDB`);
 }
 
 async function disconnectDB() {
